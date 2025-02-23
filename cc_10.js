7cc3b3fd43d664c929560261173e7791fe689cfc
Mark Francis Pablo <markfrancispablojr@gmail.com>
2025-02-22 20:45:31 -0500

    Task 2 Created order class

--- cc_10.js
+++ cc_10.js
@@ -20,5 +20,32 @@ console.log(prod1.getDetails());
 prod1.updateStock(3);
 console.log(prod1.getDetails()); 
 
-//Task 2
+//Task 2 Created order class
+class Order {
+    constructor(orderId, product, quantity) {
+      this.orderId = orderId;
+      this.product = product;
+      this.quantity = quantity;
+      this.totalPrice = this.calculateTotalPrice();
+      
+      this.product.updateStock(this.quantity);
+    }
+  
+    calculateTotalPrice() {
+      return this.product.price * this.quantity;
+    }
+  
+    getOrderDetails() {
+      return `Order ID: ${this.orderId}, Product: ${this.product.name}, Quantity: ${this.quantity}, Total Price: $${this.totalPrice}`;
+    }
+  }
 
+  const order1 = new Order(501, prod1, 2);
+console.log(order1.getOrderDetails()); 
+
+
+console.log(prod1.getDetails()); 
+
+  
+  
+  
